006d46ba9ec59d76ac665a630ef84aacf820d54e
Sergio Azevedo <sergior.jr@gmail.com>
2024-08-08 11:06:28 +0200

    contract core functionality ready

--- project/contracts/CyberNorseWarriors.sol
+++ project/contracts/CyberNorseWarriors.sol
@@ -5,85 +5,110 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "hardhat/console.sol";
 
-// shold mint be free to owner only or public and payable
-// should I have a function that allows the owner to:
-// - increase the MAX_SUPPLY value ✅
+// Desired functionalities:
+// - increase the MAX_SUPPLY value (only Owner) ✅
 // - check the MAX_SUPPLY value ✅
 // - allow the baseTokenURI to be changed so that the new token batch can use a new url for metadata ✅
 //   Example: initial supply 30 NTFs baseURI ipfs://bucket1
 //      owner bumps supply to 50 NFTs baseURI ipfs://bucket-new then:
-//          - we need to keep user ipfs://bucket1 until the initial suply is over (tokenId <= 30)
-//          - we neee to start using ipfs://bucket-new as baseURI if tokenId > 30
-// -------- look at batchnft.sol for insipiration
+//          - we need to keep user ipfs://bucket1 until the initial suply is over (tokenId <= 30) ✅
+//          - we neee to start using ipfs://bucket-new as baseURI if tokenId > 30 ✅
 
-// contract CyberNorseWarriors is ERC721, ERC721URIStorage, Ownable {
-contract CyberNorseWarriors is ERC721, Ownable {
+contract CyberNorseWarriors is ERC721, ERC721URIStorage, Ownable {
     struct NFTBatch {
-        uint256 maxSupply;
+        uint256 startFrom;
         string metadataURI;
     }
 
     event NFTBatchAdded(uint256 maxSupply, string metadataURI);
 
+    uint256 private _maxSupply;
     uint256 private _lastTokenId;
     NFTBatch private _currentBatch;
     NFTBatch private _nextBatch;
+
+    uint256 private _mintPrice = 0.003 ether;
 
     constructor(
         address deployAddress,
         uint256 initialSupply,
         string memory tokenMetadaURI
     ) ERC721("CyberNorseWarriors", "CNW") Ownable(deployAddress) {
-        _currentBatch.maxSupply = initialSupply;
+        _maxSupply = initialSupply;
+        _currentBatch.startFrom = 0;
         _currentBatch.metadataURI = tokenMetadaURI;
     }
 
     function maxSupply() public view onlyOwner returns (uint256) {
-        return _currentBatch.maxSupply;
+        return _maxSupply;
     }
 
-    function bumpSupplyAvaiable() public view onlyOwner returns (bool) {
-        return _nextBatch.maxSupply == 0;
+    function bumpSupplyAvaiable() external view onlyOwner returns (bool) {
+        return _nextBatch.startFrom == 0;
     }
 
     function bumpSupply(
-        uint256 newMaxSupply,
+        uint256 newSupplySize,
         string memory newMetadataURI
     ) public onlyOwner {
         require(
-            _nextBatch.maxSupply == 0,
+            _nextBatch.startFrom == 0,
             "Supply bumps are currently locked!"
         );
-        require(newMaxSupply > 0, "new supply must be greather than 0");
+        require(newSupplySize > 0, "new supply must be greather than 0");
         require(
             bytes(newMetadataURI).length > 0,
             "metdata URI should be defined"
         );
 
-        _nextBatch.maxSupply = newMaxSupply;
+        _nextBatch.startFrom = _maxSupply + 1;
         _nextBatch.metadataURI = newMetadataURI;
-        emit NFTBatchAdded(newMaxSupply, newMetadataURI);
+        _maxSupply += newSupplySize;
+
+        emit NFTBatchAdded(newSupplySize, newMetadataURI);
     }
 
-    // function safeMint(address to, string memory uri) public onlyOwner {
-    //     uint256 tokenId = _lastTokenId++;
-    //     _safeMint(to, tokenId);
-    //     _setTokenURI(tokenId, uri);
-    // }
+    function safeMint(address to) public payable {
+        require(
+            maxSupply() >= (_lastTokenId + 1),
+            "Sorry, all items are gone."
+        );
+        require(msg.value >= _mintPrice, "Insufient Funds");
 
-    // // The following functions are overrides required by Solidity.
+        uint256 tokenId = _lastTokenId += 1;
 
-    // function tokenURI(
-    //     uint256 tokenId
-    // ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-    //     return super.tokenURI(tokenId);
-    // }
+        // //check if we need to rotate NFT Batches
+        if ((_nextBatch.startFrom > 0) && (_nextBatch.startFrom == tokenId)) {
+            rotateCurrentBatch();
+        }
+        super._safeMint(to, tokenId);
+        super._setTokenURI(tokenId, _currentBatch.metadataURI);
+    }
 
-    // function supportsInterface(
-    //     bytes4 interfaceId
-    // ) public view override(ERC721, ERC721URIStorage) returns (bool) {
-    //     return super.supportsInterface(interfaceId);
-    // }
+    function rotateCurrentBatch() internal {
+        // rotate _currentBatch
+        _currentBatch.startFrom = _nextBatch.startFrom;
+        _currentBatch.metadataURI = _nextBatch.metadataURI;
+
+        // clears _nextBatch
+        _nextBatch.metadataURI = "";
+        _nextBatch.startFrom = 0;
+    }
+
+    //// The following functions are overrides required by Solidity.
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 }
--- project/test/CyberNorseWarriors.ts
+++ project/test/CyberNorseWarriors.ts
@@ -4,25 +4,21 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import hre from "hardhat";
 import { CyberNorseWarriors } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
-import { ContractTransactionResponse } from "ethers";
 
-interface IContract {
+interface IFixtures {
   owner: HardhatEthersSigner;
   secondAccount: HardhatEthersSigner;
   otherAccounts: Array<HardhatEthersSigner>;
-  contract: CyberNorseWarriors & {
-    deploymentTransaction(): ContractTransactionResponse;
-  }
+  contract: CyberNorseWarriors;
 };
-
 
 describe("CyberNorseWarriors", async () => {
   //Fixture
-  async function deployContract() {
+  async function deployContract() : Promise<IFixtures> {
     const contractFactory = await hre.ethers.getContractFactory("CyberNorseWarriors");
     const [owner, secondAccount, ...otherAccounts] = await ethers.getSigners();
 
-    const contract = await contractFactory.deploy(owner, 30, "ipfs://abc");
+    const contract = await contractFactory.deploy(owner, 5, "ipfs://abc");
     await contract.waitForDeployment();
 
     return {
@@ -40,7 +36,7 @@ describe("CyberNorseWarriors", async () => {
         contract,
         owner,
         secondAccount
-      } = await loadFixture<IContract>(deployContract);
+      } = await loadFixture<IFixtures>(deployContract);
 
       expect(await contract.owner()).to.equal(owner.address);
       expect(await contract.owner()).not.equal(secondAccount.address);
@@ -48,22 +44,32 @@ describe("CyberNorseWarriors", async () => {
   });
 
   describe("maxSupply", () => {
-
-    it("returns the initial supply defined during the deploy if called by the Owner address", async () => {
-      const { contract } = await loadFixture<IContract>(deployContract);
-
-      expect(await contract.maxSupply()).to.equal(30);
+    it("must fail/revert when called by a non Owner address", async () => {
+      const { contract, secondAccount } = await loadFixture<IFixtures>(deployContract);
+      await expect(contract.connect(secondAccount).maxSupply()).to.be.reverted;
     });
 
-    it("must fail/revert when called by a non Owner address", async () => {
-      const { contract, secondAccount } = await loadFixture<IContract>(deployContract);
-      await expect(contract.connect(secondAccount).maxSupply()).to.be.reverted;
+    context("after deploy", () => {
+      it("returns the supply defined during the deploy", async () => {
+        const { contract } = await loadFixture<IFixtures>(deployContract);
+
+        expect(await contract.maxSupply()).to.equal(5);
+      });
+    });
+
+    context("after a bumpSupply operation", () => {
+      it("returns the total supply (intital supply + new supply)", async () => {
+        const { contract } = await loadFixture<IFixtures>(deployContract);
+        await contract.bumpSupply(50, "ipfs://some-uri");
+
+        expect(await contract.maxSupply()).to.equal(55);
+      });
     });
   });
 
   describe("bumpSupply", () => {
     it("emits NFTBatchAdded event", async () => {
-      const { contract } = await loadFixture<IContract>(deployContract);
+      const { contract } = await loadFixture<IFixtures>(deployContract);
       await expect(
         contract.bumpSupply(10, "ipfs://zxy")
       ).to.emit(
@@ -76,26 +82,26 @@ describe("CyberNorseWarriors", async () => {
 
     context("Validations", async () => {
       it("must fail/revert when called by a non Owner address", async () => {
-        const { contract, secondAccount } = await loadFixture<IContract>(deployContract);
+        const { contract, secondAccount } = await loadFixture<IFixtures>(deployContract);
         await expect(contract.connect(secondAccount).bumpSupply(10, "ipfs://zxy")).to.be.reverted;
       });
 
       it("must fail/revert when called with max supply 0", async () => {
-        const { contract } = await loadFixture<IContract>(deployContract);
+        const { contract } = await loadFixture<IFixtures>(deployContract);
         await expect(
           contract.bumpSupply(0, "ipfs://zxy")
         ).to.be.revertedWith("new supply must be greather than 0");
       });
 
       it("must fail/revert when called with empty metdatada uri", async() => {
-        const { contract } = await loadFixture<IContract>(deployContract);
+        const { contract } = await loadFixture<IFixtures>(deployContract);
         await expect(
           contract.bumpSupply(20, "")
         ).to.be.revertedWith('metdata URI should be defined');
       });
 
       it("must fail/revert if the bumpSupply is locked", async() => {
-        const { contract } = await loadFixture<IContract>(deployContract);
+        const { contract } = await loadFixture<IFixtures>(deployContract);
         //this operation will lock bumpSupply
         await contract.bumpSupply(10, "ipfs://zxy");
 
@@ -108,18 +114,80 @@ describe("CyberNorseWarriors", async () => {
 
   describe("bumpSupplyAvaiable", () => {
     it("returns true if thre is a free batch slot avaiable", async () => {
-      const { contract } = await loadFixture<IContract>(deployContract);
+      const { contract } = await loadFixture<IFixtures>(deployContract);
       expect(
         await contract.bumpSupplyAvaiable()
       ).to.be.true
     });
 
     it("returns false if thre is no free batch slot avaiable", async () => {
-      const { contract } = await loadFixture<IContract>(deployContract);
+      const { contract } = await loadFixture<IFixtures>(deployContract);
       await contract.bumpSupply(20, "ipfs://xpto");
       expect(
         await contract.bumpSupplyAvaiable()
       ).to.be.false
     });
-  })
+  });
+
+  describe("safeMint", () => {
+    it("fails/reverts if destination account doesn't have enougth funds", async () => {
+      const { contract, secondAccount } = await loadFixture<IFixtures>(deployContract);
+
+      await expect(contract.safeMint(secondAccount, {value: 0})).to.revertedWith("Insufient Funds");
+    });
+
+    it("fails if NFT supply is depleeted", async() => {
+      const { contract, secondAccount } = await loadFixture<IFixtures>(deployContract);
+      // this code ensures that the intial supply is minted
+      for (let index = 0; index <= 4; index++) {
+        await contract.safeMint(secondAccount,{ value: ethers.parseEther("0.003") });
+      };
+
+      await expect(
+        contract.safeMint(secondAccount,{ value: ethers.parseEther("0.003") })
+      ).to.be.revertedWith("Sorry, all items are gone.")
+    });
+
+    context("When destination account has sufficient funds to mint a token", () => {
+      it("emtis Transfer event", async () => {
+        const { contract, secondAccount } = await loadFixture<IFixtures>(deployContract);
+
+        await expect(
+          contract.safeMint(secondAccount,{ value: ethers.parseEther("0.003") })
+        ).to
+          .emit(contract, "Transfer")
+          .withArgs(ethers.ZeroAddress, secondAccount, 1);
+      });
+
+      it("mints a new token that belongs to secondAccount", async () => {
+        const { contract, secondAccount } = await loadFixture<IFixtures>(deployContract);
+        await contract.safeMint(secondAccount, { value: ethers.parseEther("0.003") });
+
+        expect( await contract.ownerOf(1) ).to.be.equal( secondAccount );
+      });
+
+      it("assigns the currentBatch MetadataURI to the new minted token", async () => {
+        const { contract, secondAccount } = await loadFixture<IFixtures>(deployContract);
+        await contract.safeMint(secondAccount, { value: ethers.parseEther("0.003") });
+
+        expect( await contract.tokenURI(1) ).to.be.equal( "ipfs://abc" );
+      });
+
+      it("ensures that minted tokens will use the new batch metadatadaURI when a batch is rotated", async () => {
+        const { contract, secondAccount } = await loadFixture<IFixtures>(deployContract);
+        // this code ensures that the intial supply is minted
+        for (let index = 0; index <= 4; index++) {
+          await contract.safeMint(secondAccount,{ value: ethers.parseEther("0.003") });
+        };
+
+        await contract.bumpSupply(10, "ipfs://xyz.zk");
+        // must trigger the new batch
+        await contract.safeMint(secondAccount, { value: ethers.parseEther("0.003") });
+
+        expect( await contract.tokenURI(5) ).to.be.equal("ipfs://abc");
+        expect( await contract.tokenURI(6) ).to.be.equal("ipfs://xyz.zk");
+      });
+    });
+  });
+
 });
